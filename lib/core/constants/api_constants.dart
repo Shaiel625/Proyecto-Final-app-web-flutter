78128b764f3a317e029eeca7e16e883bbbf0502d
Shaiel688 <shaiel688@gmail.com>
2026-04-14 19:56:27 -0600

    Fix: use String.fromEnvironment for URLs

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,12 +1,19 @@
 class ApiConstants {
-  static const String backendBaseUrl = 'http://127.0.0.1:3000';
-  static const String productosBaseUrl = 'http://127.0.0.1:8010';
+  static const String _backendBaseUrl = String.fromEnvironment(
+    'BACKEND_URL',
+    defaultValue: 'http://127.0.0.1:3000',
+  );
 
-  static const String login = '$backendBaseUrl/auth/login';
-  static const String me = '$backendBaseUrl/auth/me';
-  static const String usuarios = '$backendBaseUrl/usuarios';
-  static const String clientes = '$backendBaseUrl/clientes';
-  static const String ventas = '$backendBaseUrl/ventas';
+  static const String _productosBaseUrl = String.fromEnvironment(
+    'PRODUCTOS_URL',
+    defaultValue: 'http://127.0.0.1:8010',
+  );
 
-  static const String productos = '$productosBaseUrl/productos';
+  static const String login = '$_backendBaseUrl/auth/login';
+  static const String me = '$_backendBaseUrl/auth/me';
+  static const String usuarios = '$_backendBaseUrl/usuarios';
+  static const String clientes = '$_backendBaseUrl/clientes';
+  static const String ventas = '$_backendBaseUrl/ventas';
+
+  static const String productos = '$_productosBaseUrl/productos';
 }
