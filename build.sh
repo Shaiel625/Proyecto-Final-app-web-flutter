668ac3d417fbd9288309e2185e811e1df3c669cb
Shaiel688 <shaiel688@gmail.com>
2026-04-14 19:37:55 -0600

    Fix: move build.sh to root

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e
+
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter
-flutter/bin/flutter config --enable-web
-flutter/bin/flutter pub get
-flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL --dart-define=PRODUCTOS_URL=$PRODUCTOS_URL
+export PATH="$PATH:$(pwd)/flutter/bin"
+export FLUTTER_ROOT="$(pwd)/flutter"
+
+# Allow running as root
+export FLUTTER_SUPPRESS_ANALYTICS=1
+git config --global --add safe.directory $(pwd)/flutter
+
+flutter config --enable-web --no-analytics
+flutter pub get
+flutter build web \
+  --dart-define=BACKEND_URL=$BACKEND_URL \
+  --dart-define=PRODUCTOS_URL=$PRODUCTOS_URL
